ecc45a48e0a1178ebf45f2fd8ef80c16ca5981a9
adewunmi bamishigbin <32496428+adexbam@users.noreply.github.com>
2019-07-29 09:12:00 +0200

    Integrating the Login and registeration View

--- AppData/Local/Packages/CanonicalGroupLimited.UbuntuonWindows_79rhkp1fndgsc/LocalState/rootfs/home/adexbam/movie_api/client/src/components/main-view/main-view.jsx
+++ AppData/Local/Packages/CanonicalGroupLimited.UbuntuonWindows_79rhkp1fndgsc/LocalState/rootfs/home/adexbam/movie_api/client/src/components/main-view/main-view.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import axios from 'axios';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+import Container from 'react-bootstrap/Container';
 
+import { LoginView } from '../login-view/login-view';
+import { RegistrationView } from '../registration-view/registration-view';
 import { MovieCard } from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
 
@@ -11,7 +16,9 @@ export class MainView extends React.Component {
 
     this.state = {
       movies: null,
-      selectedMovie: null
+      selectedMovie: null,
+      user: null,
+      register: false
     };
   }
 
@@ -29,23 +36,37 @@ export class MainView extends React.Component {
     });
   }
 
-  returnBtnClick() {
-  this.setState({
-    selectedMovie: null
-  });
-}
+  onLoggedIn(user) {
+    this.setState({
+      user
+    });
+  }
+
+  clickHandle() {
+    this.setState({
+      selectedMovie: null
+    });
+  }
+
+  register() {
+    this.setState({
+      register: true
+    });
+  }
+
 
 
   render() {
-    const { movies, selectedMovie } = this.state;
+    const { movies, selectedMovie, user, register } = this.state;
 
+    if (!user) return <LoginView onLoggedIn={user => this.onLoggedIn(user)} />;
     // Before the movies have been loaded
     if (!movies) return <div className="main-view"/>;
 
     return (
      <div className="main-view">
       {selectedMovie
-         ? (<MovieView movie={selectedMovie}onClick={() => this.returnBtnClick()}/>)
+         ? (<MovieView movie={selectedMovie}onClick={() => this.clickHandle()}/>)
          : (movies.map(movie => (
            <MovieCard key={movie._id} movie={movie} onClick={movie => this.onMovieClick(movie)}/>)
          ))
---
+++ AppData/Local/Packages/CanonicalGroupLimited.UbuntuonWindows_79rhkp1fndgsc/LocalState/rootfs/home/adexbam/movie_api/client/src/components/registration-view/registration-view.jsx
@@ -0,0 +1,77 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+
+import { Link } from 'react-router-dom';
+
+import './registration-view.scss';
+
+
+export function RegistrationView(props) {
+    const [username, setUsername ] = useState('');
+    const [password, setPassword] = useState('');
+    const [email, setEmail] = useState('');
+    const [birthday, setBirthday] = useState('');
+  
+    const handleSubmit = (event) => {
+      event.preventDefault();
+      axios.post('https://myflix-ade.herokuapp.com/users', {
+        Username: username,
+        Password: password,
+        EMail: email,
+        Birthday: birthday
+      })
+      .then(response => {
+        const data = response.data;
+        console.log(data);
+        window.open('/', '_self');
+      })
+      .catch(event => {
+        console.log('error registering the user')
+      });
+    };
+  
+    return (
+      <div className="registration-view">
+      <Form>
+        <h2>Sign In!</h2>
+        <Form.Group controlId="formBasicUsername">
+          <Form.Label >Your Username</Form.Label>
+          <Form.Control type="text" value={username} onChange={e => setUsername(e.target.value)} placeholder="Enter Username" />
+          <Form.Text className="text-muted">
+          Type your username here.
+          </Form.Text>
+        </Form.Group>
+  
+        <Form.Group controlId="formBasicPassword">
+          <Form.Label>Your Password</Form.Label>
+          <Form.Control type="password" value={password} onChange={e => setPassword(e.target.value)} placeholder="Password" />
+        </Form.Group>
+  
+        <Form.Group controlId="formBasicEmail">
+          <Form.Label>Your Email</Form.Label>
+          <Form.Control type="email" value={email} onChange={e => setEmail(e.target.value)} placeholder="sample@mail.de" />
+        </Form.Group>
+  
+        <Form.Group controlId="formBasicBirthday">
+          <Form.Label>Your Birthday</Form.Label>
+          <Form.Control type="text" value={birthday} onChange={e => setBirthday(e.target.value)} placeholder="01.01.2000" />
+        </Form.Group>
+  
+        <Button variant="primary" type="button" onClick={handleSubmit}>
+        SIGN IN
+        </Button>
+        <p>
+          Already a member?
+          <Link to={'/'}>
+            <span> LOG IN</span>
+          </Link>
+        </p>
+      </Form>
+      </div>
+    );
+  }
+
+
+export default RegistrationView
