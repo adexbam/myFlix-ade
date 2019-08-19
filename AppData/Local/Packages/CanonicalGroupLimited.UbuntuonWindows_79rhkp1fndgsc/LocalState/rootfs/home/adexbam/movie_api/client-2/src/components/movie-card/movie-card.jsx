6dfe9feecc79b4cf85a94f3cfb4add314f4d48f3
adewunmi bamishigbin <32496428+adexbam@users.noreply.github.com>
2019-08-19 17:40:44 +0200

    import and use link in movie-card

--- AppData/Local/Packages/CanonicalGroupLimited.UbuntuonWindows_79rhkp1fndgsc/LocalState/rootfs/home/adexbam/movie_api/client-2/src/components/movie-card/movie-card.jsx
+++ AppData/Local/Packages/CanonicalGroupLimited.UbuntuonWindows_79rhkp1fndgsc/LocalState/rootfs/home/adexbam/movie_api/client-2/src/components/movie-card/movie-card.jsx
@@ -1,20 +1,25 @@
-
 import React from "react";
 import PropTypes from 'prop-types';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 
+import { Link } from 'react-router-dom';
+
 export class MovieCard extends React.Component {
   render() {
-    const { movie, onClick } = this.props;
+    const { movie } = this.props;
 
     return (
       <Card style={{ width: '16rem' }}>
-        <Card.Img variant="top" src={movie.ImagePath} />
+        <Link to={`/movies/${movie._id}`}>
+          <Card.Img variant="top" src={movie.ImagePath} />
+        </Link>
         <Card.Body>
           <Card.Title>{movie.Title}</Card.Title>
           <Card.Text>{movie.Description}</Card.Text>
-          <Button onClick={() => onClick(movie)} variant="link">Details</Button>
+          <Link to={`/movies/${movie._id}`}>
+            <Button variant="primary">More Details</Button>
+          </Link>
           </Card.Body>
       </Card>
     );
@@ -22,7 +27,9 @@ export class MovieCard extends React.Component {
 }
 
 MovieCard.propTypes = {
-  movie: PropTypes.shape ({
-    Title: PropTypes.string
+  movie: PropTypes.shape({
+    Title: PropTypes.string,
+    Description: PropTypes.string,
+    ImagePath: PropTypes.string
   }).isRequired
-}
+};
